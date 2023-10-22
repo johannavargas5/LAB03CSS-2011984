a35b7c1d18875544bec440f068b81a5644d7c7d0
danferkz <gdanfer07@gmail.com>
2023-10-22 00:09:19 -0500

    final

--- src/tareatest21/numeroDos.java
+++ src/tareatest21/numeroDos.java
@@ -4,31 +4,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class numeroDos {
+class NumeroDos {
 
-	@Test
+    @Test
+    // Prueba para verificar si la función findHighestNumber encuentra el número más alto en el caso 1
     public void testFindHighestNumberCase1() {
         int result = tarea.findHighestNumber(3, 5, 9);
         assertEquals(9, result);
     }
 
     @Test
+    // Prueba para verificar si la función findHighestNumber encuentra el número más alto en el caso 2
     public void testFindHighestNumberCase2() {
         int result = tarea.findHighestNumber(10, 5, 1);
         assertEquals(10, result);
     }
 
     @Test
+    // Prueba para verificar si la función findHighestNumber encuentra el número más alto en el caso 3
     public void testFindHighestNumberCase3() {
         int result = tarea.findHighestNumber(4, 12, 8);
         assertEquals(12, result);
     }
 
     @Test
+    // Prueba para verificar si la función findHighestNumber lanza una excepción cuando se pasan números iguales
     public void testFindHighestNumberCase4() {
         assertThrows(IllegalArgumentException.class, () -> {
             tarea.findHighestNumber(4, 4, 4);
         });
     }
 }
-
