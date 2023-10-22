51cdb7904e0f51ca95ab981d0ab5d34ef49ac774
danielchicana <danielchicanamartinez@gmail.com>
2023-10-21 22:19:21 -0500

    danielchicana

--- src/tareatest21/numeroSeis.java
+++ src/tareatest21/numeroSeis.java
@@ -6,30 +6,24 @@ import org.junit.jupiter.api.Test;
 
 class numeroSeis {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
-}
-@test
-public void pruebaCompararNumerosIguales() {
-	int resultado = numeroSeis.compareTwoNumbers(3,3);
+    @test
+    public void pruebaCompararNumerosIguales() {
+	int resultado = tarea.compareTwoNumbers(3,3);
 	assertEquals(0,resultado);
 }
 
 
-@test
-public void pruebaCompararSegundoNumeroMayor() {
-	int resultado = numeroSeis.compareTwoNumbers(3,5);
+    @test
+    public void pruebaCompararSegundoNumeroMayor() {
+	int resultado = tarea.compareTwoNumbers(3,5);
 	assertEquals(-1,resultado);
 }
 
 
-@Test
-public void pruebaExcepcionParaNumerosNegativos() {
+    @Test
+    public void pruebaExcepcionParaNumerosNegativos() {
     try {
-        int resultado = numeroSeis.compareTwoNumbers(-2, 3);
+        int resultado = tarea.compareTwoNumbers(-2, 3);
         fail("La excepción de números negativos no se lanzó");
     } catch (IllegalArgumentException e) {
         // Verifica que la excepción lanzada sea del tipo IllegalArgumentException.
@@ -37,13 +31,16 @@ public void pruebaExcepcionParaNumerosNegativos() {
     }
 }
 
-@Test
-public void pruebaExcepcionParaNumerosIguales() {
+    @Test
+    public void pruebaExcepcionParaNumerosIguales() {
     try {
-        int resultado = numeroSeis.compareTwoNumbers(4, 4);
+        int resultado = tarea.compareTwoNumbers(4, 4);
         fail("La excepción de números iguales no se lanzó");
     } catch (ArithmeticException e) {
         // Verifica que la excepción lanzada sea del tipo ArithmeticException.
         assertEquals("Ambos números son iguales", e.getMessage());
     }
+}
+
+
 }
