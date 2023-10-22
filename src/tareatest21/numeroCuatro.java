fddd01e5c71ac5f32420d038df7c08da03379777
johannavargas5 <johanna.vargasf@usil.pe>
2023-10-21 19:54:35 -0500

    octavo commit

--- src/tareatest21/numeroCuatro.java
+++ src/tareatest21/numeroCuatro.java
@@ -1,52 +1,46 @@
 package tareatest21;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-
-class numeroCuatro {
-
-    public static int calculateCube(int n) {
-        return n * n * n;
-    }
-
-    @Test
-    public void testCalculateCube_numeroNegativo() {
-        // Entrada: n = -1
-        // Salida esperada: -1
-        int n = -1;
-        int resultadoEsperado = -1;
-        int resultadoObtenido = numeroCuatro.calculateCube(n);
-        assertEquals(resultadoEsperado, resultadoObtenido);
-    }
-
-    @Test
-    public void testCalculateCube_numeroCero() {
+class numeroCuatro{
+	@Test
+    public void testCalculateCube_cero() {
         // Entrada: n = 0
         // Salida esperada: 0
         int n = 0;
         int resultadoEsperado = 0;
-        int resultadoObtenido = numeroCuatro.calculateCube(n);
+        int resultadoObtenido = tarea.calculateCube(n);
         assertEquals(resultadoEsperado, resultadoObtenido);
     }
 
     @Test
-    public void testCalculateCube_numeroPositivo() {
+    public void testCalculateCube_negativo() {
+        // Entrada: n = -1
+        // Salida esperada: -1
+        int n = -1;
+        int resultadoEsperado = -1;
+        int resultadoObtenido = tarea.calculateCube(n);
+        assertEquals(resultadoEsperado, resultadoObtenido);
+    }
+
+    @Test
+    public void testCalculateCube_positivo() {
         // Entrada: n = 1
         // Salida esperada: 1
         int n = 1;
         int resultadoEsperado = 1;
-        int resultadoObtenido = numeroCuatro.calculateCube(n);
+        int resultadoObtenido = tarea.calculateCube(n);
         assertEquals(resultadoEsperado, resultadoObtenido);
     }
 
     @Test
     public void testCalculateCube_numeroGrande() {
         // Entrada: n = 1000
-        // Salida esperada: 1000000
+        // Salida esperada: 10^6
         int n = 1000;
-        int resultadoEsperado = 1000000;
-        int resultadoObtenido = numeroCuatro.calculateCube(n);
+        int resultadoEsperado = 1000 * 1000 * 1000;
+        int resultadoObtenido = tarea.calculateCube(n);
         assertEquals(resultadoEsperado, resultadoObtenido);
     }
 }
