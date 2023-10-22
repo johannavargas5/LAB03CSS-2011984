b8436f04ffaced3b435a24eb68b7a797d1587da6
danielchicana <danielchicanamartinez@gmail.com>
2023-10-21 21:20:22 -0500

    danielchicana

--- src/tareatest21/numeroSeis.java
+++ src/tareatest21/numeroSeis.java
@@ -12,3 +12,38 @@ class numeroSeis {
 	}
 
 }
+@test
+public void pruebaCompararNumerosIguales() {
+	int resultado = numeroSeis.compareTwoNumbers(3,3);
+	assertEquals(0,resultado);
+}
+
+
+@test
+public void pruebaCompararSegundoNumeroMayor() {
+	int resultado = numeroSeis.compareTwoNumbers(3,5);
+	assertEquals(-1,resultado);
+}
+
+
+@Test
+public void pruebaExcepcionParaNumerosNegativos() {
+    try {
+        int resultado = numeroSeis.compareTwoNumbers(-2, 3);
+        fail("La excepción de números negativos no se lanzó");
+    } catch (IllegalArgumentException e) {
+        // Verifica que la excepción lanzada sea del tipo IllegalArgumentException.
+        assertEquals("Los números no pueden ser negativos", e.getMessage());
+    }
+}
+
+@Test
+public void pruebaExcepcionParaNumerosIguales() {
+    try {
+        int resultado = numeroSeis.compareTwoNumbers(4, 4);
+        fail("La excepción de números iguales no se lanzó");
+    } catch (ArithmeticException e) {
+        // Verifica que la excepción lanzada sea del tipo ArithmeticException.
+        assertEquals("Ambos números son iguales", e.getMessage());
+    }
+}
