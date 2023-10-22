0a8c2b06119c2ebd396577001ad4c41ed06cca19
johannavargas5 <johanna.vargasf@usil.pe>
2023-10-21 19:03:54 -0500

    sexto commit

--- src/tareatest21/numeroCuatro.java
+++ src/tareatest21/numeroCuatro.java
@@ -6,9 +6,47 @@ import org.junit.jupiter.api.Test;
 
 class numeroCuatro {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    public static int calculateCube(int n) {
+        return n * n * n;
+    }
 
+    @Test
+    public void testCalculateCube_numeroNegativo() {
+        // Entrada: n = -1
+        // Salida esperada: -1
+        int n = -1;
+        int resultadoEsperado = -1;
+        int resultadoObtenido = numeroCuatro.calculateCube(n);
+        assertEquals(resultadoEsperado, resultadoObtenido);
+    }
+
+    @Test
+    public void testCalculateCube_numeroCero() {
+        // Entrada: n = 0
+        // Salida esperada: 0
+        int n = 0;
+        int resultadoEsperado = 0;
+        int resultadoObtenido = numeroCuatro.calculateCube(n);
+        assertEquals(resultadoEsperado, resultadoObtenido);
+    }
+
+    @Test
+    public void testCalculateCube_numeroPositivo() {
+        // Entrada: n = 1
+        // Salida esperada: 1
+        int n = 1;
+        int resultadoEsperado = 1;
+        int resultadoObtenido = numeroCuatro.calculateCube(n);
+        assertEquals(resultadoEsperado, resultadoObtenido);
+    }
+
+    @Test
+    public void testCalculateCube_numeroGrande() {
+        // Entrada: n = 1000
+        // Salida esperada: 1000000
+        int n = 1000;
+        int resultadoEsperado = 1000000;
+        int resultadoObtenido = numeroCuatro.calculateCube(n);
+        assertEquals(resultadoEsperado, resultadoObtenido);
+    }
 }
--- src/tareatest21/numeroTres.java
+++ src/tareatest21/numeroTres.java
@@ -6,9 +6,56 @@ import org.junit.jupiter.api.Test;
 
 class numeroTres {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	// 3
+	public static int findMaxNumber(int arr[]) {
+		int max = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (max < arr[i])
+				max = arr[i];
+		}
+		return max;
 	}
 
+	public class tarea {
+
+	    @Test
+	    public void testFindMaxNumber_arrayVacio() {
+	        // Entrada: arr = []
+	        // Salida esperada: -1
+	        int[] arr = {};
+	        int resultadoEsperado = -1;
+	        int resultadoObtenido = numeroTres.findMaxNumber(arr);
+	        assertEquals(resultadoEsperado, resultadoObtenido);
+	    }
+
+	    @Test
+	    public void testFindMaxNumber_unElemento() {
+	        // Entrada: arr = [1]
+	        // Salida esperada: 1
+	        int[] arr = {1};
+	        int resultadoEsperado = 1;
+	        int resultadoObtenido = numeroTres.findMaxNumber(arr);
+	        assertEquals(resultadoEsperado, resultadoObtenido);
+	    }
+
+	    @Test
+	    public void testFindMaxNumber_variosElementos() {
+	        // Entrada: arr = [1, 2, 3]
+	        // Salida esperada: 3
+	        int[] arr = {1, 2, 3};
+	        int resultadoEsperado = 3;
+	        int resultadoObtenido = numeroTres.findMaxNumber(arr);
+	        assertEquals(resultadoEsperado, resultadoObtenido);
+	    }
+
+	    @Test
+	    public void testFindMaxNumber_todosLosElementosIguales() {
+	        // Entrada: arr = [1, 1, 1]
+	        // Salida esperada: 1
+	        int[] arr = {1, 1, 1};
+	        int resultadoEsperado = 1;
+	        int resultadoObtenido = numeroTres.findMaxNumber(arr);
+	        assertEquals(resultadoEsperado, resultadoObtenido);
+	    }
+	}
 }
